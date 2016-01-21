42024a150aa41639471b390e7e932b4c614a0f7b
Tiphaine MANET <tmanet@e2r11p4.42.fr>
2016-01-21 18:31:51 +0100

    conv p ok mais leak surement, gitignore added

--- ft_conv_p.c
+++ ft_conv_p.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/21 14:20:35 by tmanet            #+#    #+#             */
-/*   Updated: 2016/01/21 14:21:29 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/01/21 18:25:30 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 char	*ft_conv_p(t_str_conv *sub, va_list ap)
 {
-	sub->str_out = ft_uitoa_base(va_arg(ap, unsigned int), 16, 'a');
+	sub->str_out = ft_strjoin("0x",
+			ft_ultoa_base(va_arg(ap, unsigned long), 16, 'a'));
 	sub->size = ft_strlen(sub->str_out);
 	return (sub->str_out);
 }
