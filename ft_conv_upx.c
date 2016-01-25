c5f96fb8fda41d89bf7e8ed5720fe29991264127
Tiphaine MANET <tmanet@e1r7p6.42.fr>
2016-01-25 13:01:32 +0100

    ajout de l et ll sur x et X

--- ft_conv_upx.c
+++ ft_conv_upx.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/01/19 18:32:57 by tmanet            #+#    #+#             */
-/*   Updated: 2016/01/19 18:45:25 by tmanet           ###   ########.fr       */
+/*   Created: 2016/01/25 13:00:15 by tmanet            #+#    #+#             */
+/*   Updated: 2016/01/25 13:00:39 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 char	*ft_conv_upx(t_str_conv *sub, va_list ap)
 {
-	sub->str_out = ft_uitoa_base(va_arg(ap, unsigned int), 16, 'A');
+	if (ft_strequ("ll", sub->type))
+		sub->str_out = ft_ulltoa_base(va_arg(ap, unsigned long long), 16, 'A');
+	else if (ft_strequ("l", sub->type))
+		sub->str_out = ft_ultoa_base(va_arg(ap, unsigned long), 16, 'A');
+	else
+		sub->str_out = ft_uitoa_base(va_arg(ap, unsigned int), 16, 'A');
 	sub->size = ft_strlen(sub->str_out);
 	return (sub->str_out);
 }
