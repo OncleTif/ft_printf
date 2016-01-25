732e951cc774125bfceadd8e9e075f29628deae2
Tiphaine MANET <tmanet@e1r7p6.42.fr>
2016-01-25 12:41:34 +0100

    ajout de ll sur d

--- ft_conv_d.c
+++ ft_conv_d.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/19 17:48:38 by tmanet            #+#    #+#             */
-/*   Updated: 2016/01/19 17:53:47 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/01/25 12:40:01 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 char	*ft_conv_d(t_str_conv *sub, va_list ap)
 {
+	if (ft_strequ("ll", sub->type))
+	sub->str_out = ft_lltoa_base(va_arg(ap, long long), 10);
+			else
 	sub->str_out = ft_itoa(va_arg(ap, int));
 	sub->size = ft_strlen(sub->str_out);
 	return (sub->str_out);
