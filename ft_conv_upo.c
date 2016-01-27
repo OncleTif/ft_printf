9a99217be0560e53d0085e536674ce22dd8a9b27
tmanet <tmanet@student.42.fr>
2016-01-27 11:45:01 +0100

    correction de l'ordre entre # et width sur upo

--- ft_conv_upo.c
+++ ft_conv_upo.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/20 18:14:03 by tmanet            #+#    #+#             */
-/*   Updated: 2016/01/27 11:24:17 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/01/27 11:44:27 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,13 @@ char	*ft_conv_upo(t_str_conv *sub, va_list ap)
 		sub->str_out = ft_uitoa_base((unsigned short)va_arg(ap, unsigned int), 8, 'A');
 	else
 		sub->str_out = ft_ultoa_base(va_arg(ap, long), 8, 'A');
-	if (sub->hash && sub->str_out[0] != '0')
-		sub->str_out = ft_strjoin("0", sub->str_out);
 	sub->size = ft_strlen(sub->str_out);
 	sub->plus = 0;
 	sub->space = 0;
 	if (sub->point)
 		ft_prec_modifier(sub);
+	if (sub->hash && sub->str_out[0] != '0')
+		sub->str_out = ft_strjoin("0", sub->str_out);
+	sub->size = ft_strlen(sub->str_out);
 	return (sub->str_out);
 }
