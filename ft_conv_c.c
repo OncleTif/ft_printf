b25f787d154b809fda21df3f55ef226121982a65
tmanet <tmanet@student.42.fr>
2016-01-28 16:50:04 +0100

    ajout de h qui donne C

--- ft_conv_c.c
+++ ft_conv_c.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/20 15:47:23 by tmanet            #+#    #+#             */
-/*   Updated: 2016/01/27 18:13:14 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/01/28 16:49:07 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,15 @@
 
 char	*ft_conv_c(t_str_conv *sub)
 {
-	sub->str_out = ft_strnew(1);
-	sub->str_out[0] = (char)va_arg(sub->ap, int);
-	sub->size = 1;
-	sub->plus = 0;
-	sub->space = 0;
+	if (ft_strequ("l", sub->type))
+		sub->str_out = ft_conv_upc(sub);
+	else
+	{
+		sub->str_out = ft_strnew(1);
+		sub->str_out[0] = (char)va_arg(sub->ap, int);
+		sub->size = 1;
+		sub->plus = 0;
+		sub->space = 0;
+	}
 	return (sub->str_out);
 }
