c2d994deeebf26a0c09bff0154f285accedf83d5
tmanet <tmanet@student.42.fr>
2016-01-29 15:25:09 +0100

    ajout de erreur quand char > a taille possible

--- ft_conv_upc.c
+++ ft_conv_upc.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/22 12:43:12 by tmanet            #+#    #+#             */
-/*   Updated: 2016/01/28 16:44:20 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/01/29 15:23:52 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,12 @@ char	*ft_conv_upc(t_str_conv *sub)
 
 	wide = va_arg(sub->ap, wchar_t);
 	if ((sub->str_out = ft_wchartostr(wide)))
+	{
 		if (!(sub->size = ft_strlen(sub->str_out)))
 			sub->size = 1;
+	}
+	else
+		sub->error = 1;
 	sub->plus = 0;
 	sub->space = 0;
 	return (sub->str_out);
